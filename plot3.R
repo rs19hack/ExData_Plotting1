0b0e036fcdcfdd03c4608a242e3eea093075129e
Rajwinder Singh <gitreset@gmail.com>
2014-07-13 14:48:12 -0700

    Added code to process zip file.

--- plot3.R
+++ plot3.R
@@ -1,10 +1,21 @@
+# Uncompress the zip file downloaded with Assignment 1 instructions. The file is in the working directory.
+
+unzip("household_power_consumption.zip")
+
 rawfile <- "household_power_consumption.txt"
 
+# Read in the data from the uncompredded raw data:
 d <- read.table(rawfile, header=T, sep=';', na.strings="?", colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+
+# Select the two days of interest, as specified in assignment instructions.
 
 d2 <- d[(d$Date == "1/2/2007") | (d$Date == "2/2/2007"),]
 
+# Add new column with POSIX dat/time format to facilitate analysis & plotting.
+
 d2$DateTime <- strptime(paste(d2$Date, d2$Time), "%d/%m/%Y %H:%M:%S")
+
+# Create our plot of interest:
 
 png(filename = "plot3.png", width = 480, height = 480, units = "px")
 
